3ab89eac000a331b94ecdce4d51bb8ea8f2c267e
Diego Rivera <diegorivera1827@gmail.com>
2024-02-19 11:29:23 -0600

    Hub updates
    Horse to teleport to hub
    
    Hub area
       - Shop that opens menu when inside
    
    Bounty board sprite created

---
+++ Assets/Code/ProximityText.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ProximityText : MonoBehaviour
+{
+    public Transform player;
+    public float interactionDistance = 3f;
+    public GameObject textCanvas;
+
+    private void Start()
+    {
+        // Ensure the textCanvas is initially disabled
+        textCanvas.SetActive(false);
+    }
+
+    private void Update()
+    {
+        // Check the distance between the player and this object
+        float distanceToPlayer = Vector3.Distance(transform.position, player.position);
+
+        // Show/hide the textCanvas based on the interactionDistance
+        if (distanceToPlayer <= interactionDistance)
+        {
+            // Display interaction text and enable canvas
+            textCanvas.SetActive(true);
+        }
+        else
+        {
+            // Hide interaction text and disable canvas
+            textCanvas.SetActive(false);
+        }
+    }
+}
