71737b2010278ddf057326b58540f10fdd4ccdff
Diego Rivera <diegorivera1827@gmail.com>
2024-03-04 17:52:57 -0600

    Clearing Damage Flash

--- Assets/Code/GunGoon.cs
+++ Assets/Code/GunGoon.cs
@@ -1,4 +1,3 @@
-using BarthaSzabolcs.Tutorial_SpriteFlash;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
@@ -20,17 +19,13 @@ public class GunGoon : MonoBehaviour
     private Vector2 HeadingToPlayer => OffsetToPlayer.normalized;
     public float initialDelay = 0.75f;  // Delay before firing for goons
     private bool hasStartedShooting = false;
-    //private SimpleFlash flashEffect;
-    //private SpriteRenderer[] childRenderers;
+
 
 
 
     private void Start()
     {
-        // Get all SpriteRenderer components in children (including the parent itself)
-        //SpriteRenderer[] childRenderers = GetComponentsInChildren<SpriteRenderer>(true);
         timer = Time.time;
-        //flashEffect = GetComponent<SimpleFlash>();
     }
     // Update is called once per frame
     void Update()
@@ -81,11 +76,6 @@ public class GunGoon : MonoBehaviour
     public void decHealth(int hp)
     {
         health -= hp;
-        /*foreach(SpriteRenderer childRenderer in childRenderers)
-        {
-            flashEffect.Flash();
-        }
-        */
         if (health <= 0)
         {
             Instantiate(moneyPrefab, transform.position, Quaternion.identity);
